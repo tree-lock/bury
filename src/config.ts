ec423607a98078adf377d01db6b60b8c77f9d25d
Xmo <18851989097@163.com>
2022-12-12 19:14:36 +0800

    refactor: 💡 删除ip地址和cityName（因为原sohu地址已无法获取到）

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,6 @@ export interface BuryConfig {
   referrer?: string;
   width?: string;
   height?: string;
-  ip?: string;
-  cityName?: string;
   isPhone?: "phone" | "pc";
   userId?: string;
   apiUrl?: string;
@@ -17,31 +15,6 @@ export interface BuryConfig {
   pageStayTime?: string;
   [K: string]: string;
 }
-
-declare var returnCitySN: {
-  cid: string;
-  cip: string;
-  cname: string;
-};
-
-const info: Promise<{
-  cid: string;
-  cip: string;
-  cname: string;
-}> = new Promise((resolve) => {
-  const script = document.createElement("script");
-  script.src = "https://pv.sohu.com/cityjson?ie=utf-8";
-  script.async = true;
-  const head = document.getElementsByTagName("head")[0];
-  const s =
-    head.getElementsByTagName("script")[
-      head.getElementsByTagName("script").length - 1
-    ];
-  s.parentNode?.insertBefore(script, s);
-  script.onload = function () {
-    resolve(returnCitySN);
-  };
-});
 
 let config: BuryConfig = {};
 
@@ -51,9 +24,6 @@ async function initBuryConfig(loadConfig: BuryConfig) {
   const fp = await fpPromise;
   const result = await fp.get();
   const userId = result.visitorId;
-  const { cip: ip, cname: cityName } = await info;
-  config.ip = ip;
-  config.cityName = cityName;
   config.userId = userId;
   config.ua = navigator.userAgent;
   config.referrer = document.referrer;
